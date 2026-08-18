c363487e2cfa3dbaa689bcd922ba2ecd17237fc3
Thiago Monteiro <thiagomonteiro.mont@gmail.com>
2026-08-18 19:00:16 -0300

    Remove unnecessary code for now

--- Interview/Start/AppDelegate.swift
+++ Interview/Start/AppDelegate.swift
@@ -7,14 +7,4 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
-
-    func application(
-        _ application: UIApplication,
-        configurationForConnecting connectingSceneSession: UISceneSession,
-        options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        UISceneConfiguration(
-            name: "Default Configuration",
-            sessionRole: connectingSceneSession.role
-        )
-    }
 }
