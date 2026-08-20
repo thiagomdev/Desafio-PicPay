248c22b46e5dc8247677f8fc9bd5255426b5cb50
Thiago Monteiro <thiagomonteiro.mont@gmail.com>
2026-08-20 08:02:57 -0300

    Make some refactories

--- InterviewTests/Shared/HTTPClient/URLSessionHTTPClientTests.swift
+++ InterviewTests/Shared/HTTPClient/URLSessionHTTPClientTests.swift
@@ -14,43 +14,41 @@ final class URLSessionHTTPClientTests: XCTestCase {
 
     func test_execute_performsGETRequestWithGivenURL() async {
         let exp = expectation(description: "Wait for request")
-        URLProtocolStub.stub(data: anyData(), response: anyHTTPURLResponse(), error: nil)
+        URLProtocolStub.stub(data: anyData, response: anyHTTPURLResponse, error: nil)
 
         URLProtocolStub.observeRequests { request in
-            XCTAssertEqual(request.url, self.anyURL())
+            XCTAssertEqual(request.url, self.anyURL)
             XCTAssertEqual(request.httpMethod, "GET")
             exp.fulfill()
         }
 
-        _ = try? await makeSut().execute(URLRequest(url: anyURL()))
+        _ = try? await makeSut().execute(URLRequest(url: anyURL))
 
         await fulfillment(of: [exp], timeout: 1.0)
     }
 
     func test_execute_whenRequestFails_throwsTheUnderlyingError() async {
-        let requestError = anyNSError()
-        URLProtocolStub.stub(data: nil, response: nil, error: requestError)
+        URLProtocolStub.stub(data: nil, response: nil, error: anyNSError)
 
         do {
-            _ = try await makeSut().execute(URLRequest(url: anyURL()))
+            _ = try await makeSut().execute(URLRequest(url: anyURL))
             XCTFail("Expected execute to throw")
         } catch {
             let receivedError = error as NSError
-            XCTAssertEqual(receivedError.domain, requestError.domain, "Should propagate the underlying error's domain")
-            XCTAssertEqual(receivedError.code, requestError.code, "Should propagate the underlying error's code")
+            XCTAssertEqual(receivedError.domain, anyNSError.domain, "Should propagate the underlying error's domain")
+            XCTAssertEqual(receivedError.code, anyNSError.code, "Should propagate the underlying error's code")
         }
     }
 
     func test_execute_onSuccessWithHTTPURLResponse_deliversDataAndResponse() async throws {
-        let data = anyData()
-        let response = anyHTTPURLResponse()
-        URLProtocolStub.stub(data: data, response: response, error: nil)
+        let response = anyHTTPURLResponse
+        URLProtocolStub.stub(data: anyData, response: anyHTTPURLResponse, error: nil)
 
-        let result = try await makeSut().execute(URLRequest(url: anyURL()))
+        let result = try await makeSut().execute(URLRequest(url: anyURL))
 
         switch result {
         case let .success(receivedData, receivedResponse):
-            XCTAssertEqual(receivedData, data, "Should deliver the data returned by the session")
+            XCTAssertEqual(receivedData, anyData, "Should deliver the data returned by the session")
             XCTAssertEqual(receivedResponse.url, response.url, "Should deliver the response returned by the session")
             XCTAssertEqual(receivedResponse.statusCode, response.statusCode, "Should deliver the response returned by the session")
         case .failure:
@@ -59,9 +57,9 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
 
     func test_execute_whenResponseIsNotHTTP_deliversInvalidDataFailure() async throws {
-        URLProtocolStub.stub(data: anyData(), response: nonHTTPURLResponse(), error: nil)
+        URLProtocolStub.stub(data: anyData, response: nonHTTPURLResponse(), error: nil)
 
-        let result = try await makeSut().execute(URLRequest(url: anyURL()))
+        let result = try await makeSut().execute(URLRequest(url: anyURL))
 
         switch result {
         case .success:
@@ -79,24 +77,24 @@ extension URLSessionHTTPClientTests {
         return sut
     }
 
-    private func anyURL() -> URL {
+    private var anyURL: URL {
         URL(string: "https://any-url.com")!
     }
 
-    private func anyData() -> Data {
+    private var anyData: Data {
         Data("any data".utf8)
     }
 
-    private func anyNSError() -> NSError {
+    private var anyNSError: NSError {
         NSError(domain: "any error", code: 1)
     }
 
-    private func anyHTTPURLResponse() -> HTTPURLResponse {
-        HTTPURLResponse(url: anyURL(), statusCode: 200, httpVersion: nil, headerFields: nil)!
+    private var anyHTTPURLResponse: HTTPURLResponse {
+        HTTPURLResponse(url: anyURL, statusCode: 200, httpVersion: nil, headerFields: nil)!
     }
 
     private func nonHTTPURLResponse() -> URLResponse {
-        URLResponse(url: anyURL(), mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
+        URLResponse(url: anyURL, mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
     }
 
     fileprivate final class URLProtocolStub: URLProtocol {
